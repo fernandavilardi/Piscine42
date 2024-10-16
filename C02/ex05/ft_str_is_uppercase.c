f48ab51d4a51ad7391b72d83c8f0b02a44cb2ef4
Fernanda Vilardi <fernandavilardi@gmail.com>
2024-10-16 12:10:20 +0100

    ex05

---
+++ C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i = 0;
+	
+	while(str[i] != '\0')
+	{
+		if ((str[i] < 'A') || (str[i] > 'Z'))
+		return 0;
+		i++;
+	}
+	return 1;
+}
+
+/*
+int	main(void)
+{
+	printf("%d", ft_str_is_uppercase(""));
+	return 0;
+}
+*/
