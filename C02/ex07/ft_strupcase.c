ccb3a8215cfcf29fd9de020f48438ad484b66023
Fernanda Vilardi <fernandavilardi@gmail.com>
2024-10-16 12:29:35 +0100

    ex07

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i = 0;
+	
+	while(str[i] != '\0')
+	{
+		if ((str[i] > 97) && (str[i] < 122))
+		str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
+
+/*
+int	main(void)
+{
+	char	str[] = "hell is other people";
+	
+	printf("%s", ft_strupcase(str));
+	return 0;
+}
+*/
