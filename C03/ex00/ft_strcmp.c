e4ec4fc55dcde7a646429fd1fc563470371a64f7
Fernanda Vilardi <fernandavilardi@gmail.com>
2024-10-23 17:41:24 +0100

    ex00

---
+++ C03/ex00/ft_strcmp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i = 0;
+
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(void)
+{
+	char*	first_str = "dcba";
+	char*	second_str = "abcd";
+
+	printf("First string: %s\n", first_str);
+	printf("Second string: %s\n", second_str);
+	printf("Return value of strcmp(): %d", ft_strcmp(first_str, second_str));
+	return 0;
+}
