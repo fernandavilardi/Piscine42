f8a51867bdb1c3311e66f07458e44f0b610323fa
Fernanda Vilardi <fernandavilardi@gmail.com>
2024-10-23 17:58:06 +0100

    ex02

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *scr)
+{
+	int	i = 0;
+	int	j = 0;
+
+	while (dest[i] != '\0')
+	i++;
+	{
+		while (scr[j] != '\0')
+		{
+			dest [i + j] = scr[j];
+			j++;
+		}
+		dest[i + j] = '\0';
+		return (dest);
+	}
+}
+
+int	main(void)
+{
+	char	dest[50] = "oi!";
+	char	scr[50] = "Bom dia!";
+
+	ft_strcat(dest, scr);
+	printf("dest: %s\n", dest);
+	return 0;
+}
