44c1676cd369e4dc5a90b1b1113eb61d62362e33
Fernanda Vilardi <fernandavilardi@gmail.com>
2024-10-24 15:27:34 +0100

    ex03

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i = 0;
+	unsigned int	j = 0;
+
+	while (dest[i] != '\0')
+	i++;
+	{
+		while (src[j] != '\0' && j < nb)
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
+		}
+		dest[i] = '\0';
+		return (dest);
+	}
+}
+
+int	main(void)
+{
+	char	dest[10] = "Gilmore ";
+	char	src[10] = "Girls";
+	char	*nb = ft_strncat(dest, src, 10);
+	
+	printf("%s", nb);
+	return 0;
+}
