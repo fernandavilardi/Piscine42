bf210b718fc9a604627de4df2f836e136c6f6e4c
Fernanda Vilardi <fernandavilardi@gmail.com>
2024-10-24 15:56:07 +0100

    ex01

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -22,6 +22,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	}
 }
 
+/*
 int	main(void)
 {
 	char	s1[15] = "abcde";
@@ -35,3 +36,4 @@ int	main(void)
 	printf("Return value(): %d", ft_strncmp(s1, s2, result));
 	return 0;
 }
+*/
