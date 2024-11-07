96eba44a1c7f46ab3aedf63d6e9824a8e531d724
Fernanda Vilardi <fernandavilardi@gmail.com>
2024-11-07 15:54:46 +0000

    ex10

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -10,26 +10,29 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	}
 	if (size > 0)
 	{
-		unsigned int	j;
+		unsigned int	j = 0;
 		
-		for (j = 0; j < size - 1 && src[j] != '\0'; j++)
+		while (j < size - 1 && src[j] != '\0')
 		{
 			dest[j] = src[j];
+			j++;
 		}
 		dest[j] = '\0';
 	}
 	return i;
 }
 
+/*
 int	main(void)
 {
-	char	src[] = "the cake is a lie";
-	char	dest[20];
+	char src[] = "the cake is a lie";
+	char dest[20];
 
 	printf("src: %s\n", src);
 	unsigned int	len = ft_strlcpy(dest, src, sizeof(dest));
-	
+
 	printf("dest: %s\n", dest);
-	printf("comprimento de src: %u\n", len);
+	printf("Comprimento de src: %u\n", len);
 	return 0;
 }
+*/
